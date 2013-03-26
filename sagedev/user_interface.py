2f516e0af3c2aac0a331ada78320dfe598f66dfa
David Roe <roed.math@gmail.com>
2013-03-25 20:25:30 -0700

    Split off UserInterface

---
+++ sagedev/user_interface.py
@@ -0,0 +1,72 @@
+
+
+class CmdLineInterface(object):
+    def get_input(self, prompt, options=None, default=None, dryrun=False):
+        """
+        Get input from the developer.
+
+        INPUT:
+
+        - ``promt`` -- a string
+        - ``options`` -- a list of strings or None
+        - ``default`` -- a string or None
+        - ``dryrun`` -- boolean
+
+        EXAMPLES::
+
+            sage: SD = SageDev()
+            sage: SD.get_input("Should I delete your home directory?",
+            ... ["yes","no"], default="y", dryrun=True)
+            'Should I delete your home directory? [Yes/no] '
+        """
+        if default is not None:
+            for i, opt in enumerate(options):
+                if opt.startswith(default):
+                    default = i
+                    break
+            else:
+                default = None
+        if options is not None:
+            options = list(options)
+            if len(options) > 0:
+                for i in range(len(options)):
+                    if i == default:
+                        options[i] = str(options[i]).capitalize()
+                    else:
+                        options[i] = str(options[i]).lower()
+                prompt += " [" + "/".join(options) + "] "
+                options[default] = options[default].lower()
+            else:
+                prompt += " "
+                options = None
+        if dryrun:
+            return prompt
+        while True:
+            s = raw_input(prompt)
+            if options is None:
+                return s
+            if len(s.strip()) == 0:
+                if default is None:
+                    print "Please enter an option"
+                    continue
+                else:
+                    return options[default]
+            found = -1
+            for i, opt in enumerate(options):
+                if opt.startswith(s):
+                    if found == -1:
+                        found = i
+                    else:
+                        break
+            else:
+                if found != -1:
+                    return options[found]
+            if found == -1:
+                print "Please specify an allowable option"
+            else:
+                print "Please disambiguate between options"
+
+    def confirm(self, question, default_yes=True):
+        ok = self.get_input(question, ["yes","no"],
+                            "yes" if default_yes else "no")
+        return ok == "yes"
