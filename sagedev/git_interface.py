4594747cce1c425ecbaffaf3022adedb94c4a38b
Keshav Kini <keshav.kini@gmail.com>
2013-03-26 16:57:18 -0700

    Implement some low-level git utility functions

--- sagedev/git_interface.py
+++ sagedev/git_interface.py
@@ -1,4 +1,4 @@
-from subprocess import call
+from subprocess import call, check_output, CalledProcessError
 import types
 
 class GitInterface(object):
@@ -18,7 +18,7 @@ class GitInterface(object):
         # for now, no error checking
         return str(s)
 
-    def execute(self, cmd, *args, **kwds):
+    def _run_git(self, output_type, cmd, args, kwds):
         s = self._gitcmd + " " + cmd
         dryrun = kwds.pop("dryrun", None)
         for k, v in kwds.iteritems():
@@ -35,11 +35,28 @@ class GitInterface(object):
         if dryrun:
             return s
         else:
-            return call(s, shell=True)
+            if output_type == 'retval':
+                return call(s, shell=True)
+            elif output_type == 'stdout':
+                return check_output(s, shell=True)
+
+    def execute(self, cmd, *args, **kwds):
+        return self._run_git('retval', cmd, args, kwds)
+
+    def execute_silent(self, cmd, *args, **kwds):
+        return self._run_git('retval', cmd, args + ('>/dev/null',), kwds)
+
+    def read_output(self, cmd, *args, **kwds):
+        return self._run_git('stdout', cmd, args, kwds)
+
+
+    def is_ancestor_of(self, a, b):
+        revs = self.read_output('rev-list', '{}..{}'.format(b, a)).splitlines()
+        return len(revs) == 0
 
     def has_uncommitted_changes(self):
-        # Returns True if there are uncommitted changes or non-added files
-        raise NotImplementedError
+        # Returns True if there are uncommitted changes
+        return self.execute('diff', quiet=True) != 0
 
     def commit_all(self, *args, **kwds):
         # if files are non-tracked and user doesn't want to add any of
@@ -47,14 +64,17 @@ class GitInterface(object):
         kwds['a'] = True
         self.execute("commit", *args, **kwds)
 
-    def files_added(self):
+    def unknown_files(self):
         # Should return a list of filenames of files that the user
         # might want to add
-        raise NotImplementedError
+        status_output = self.read_output("status", porcelain=True)
+        files = [line[3:] for line in status_output.splitlines()
+                          if line[:2] == '??']
+        return files
 
     def add_file(self, F):
         # Should add the file with filename F
-        raise NotImplementedError
+        self.execute('add', "'{}'".format(F))
 
     def save(self):
         diff = self.UI.confirm("Would you like to see a diff of the changes?",
@@ -68,6 +88,21 @@ class GitInterface(object):
                 self.add_file(F)
         msg = self.UI.get_input("Please enter a commit message:")
         self.commit_all(m=msg)
+
+    def local_branches(self):
+        raise NotImplementedError
+
+    def current_branch(self):
+        try:
+            branch = self.read_output('symbolic-ref', 'HEAD').strip()
+            if not branch.startswith('refs/heads/'):
+                raise RuntimeError('HEAD is bizarre!')
+            return branch[11:]
+        except CalledProcessError:
+            return None
+
+    def branch_exists(self, branch):
+        return NotImplementedError
 
     def create_branch(self, branchname, at_unstable=False):
         if at_unstable:
@@ -100,7 +135,7 @@ class GitInterface(object):
 def git_cmd_wrapper(git_cmd, interface):
     def f(self, *args, **kwds):
         return self.execute(git_cmd, *args, **kwds)
-    return type.MethodType(f, interface, interface)
+    return types.MethodType(f, interface, interface)
 
 for git_cmd in ["add","bisect","branch","checkout",
                "clone","commit","diff","fetch",
